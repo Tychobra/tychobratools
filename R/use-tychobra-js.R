4182832394bb43c3cbde318e255d68f71d0a98f7
merlinoa <merlinoa88@gmail.com>
2018-01-11 16:20:11 -0500

    one more try

--- R/use-tychobra-js.R
+++ R/use-tychobra-js.R
@@ -17,6 +17,10 @@ use_tychobra_js <- function() {
           shiny::tags$link(rel="icon", href="http://res.cloudinary.com/dxqnb8xjb/image/upload/c_scale,w_20/v1510505618/tychobra-logo-blue_d2k9vt.png"),
           shiny::tags$script(
             src = file.path("tychobratools", "tychobra-shinydashboard.js")
+          ),
+          shiny::tags$link(
+            rel = "stylesheet",
+            href = file.path("tychobratools", "loading.css")
           )
         )
       )
