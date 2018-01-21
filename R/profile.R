57b85705ddf490cf2c6e4a6b87fbd13a675df104
merlinoa <merlinoa88@gmail.com>
2018-01-21 08:50:58 -0500

    standard logout button for cloud

--- R/profile.R
+++ R/profile.R
@@ -57,3 +57,43 @@ profile_icon_ui <- function(id) {
     )
   )
 }
+
+
+#' profile_icon_cloud_ui
+#'
+#' ui code for `profile_icon` for cloud.tychobra.com. Should be placed in the the `header`
+#' argument to `dashboardPage()`
+#'
+#' @param id character string identifying the module
+#'
+#' @export
+profile_icon_cloud_ui <- function(id) {
+  ns <- NS(id)
+
+  tags$li(
+    class = "dropdown",
+    tags$a(
+      href="#",
+      class = "dropdown-toggle",
+      `data-toggle` = "dropdown",
+      tags$i(
+        class = "fa fa-user"
+      )
+    ),
+    tags$ul(
+      class = "dropdown-menu",
+      tags$li(
+        textOutput(ns("auth_user")),
+        style='padding: 3px 20px;'
+      ),
+      tags$li(
+        a(
+          icon("sign-out"),
+          "Logout",
+          href="#",
+          id = "logout_button"
+        )
+      )
+    )
+  )
+}
