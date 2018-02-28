86d046fd7466b10bbb47b4fdf43d477250f6f243
merlinoa <merlinoa88@gmail.com>
2018-02-28 17:14:50 -0500

    removed link to cloud.tychobra.com from logo

--- R/shinydashboard-title.R
+++ R/shinydashboard-title.R
@@ -11,14 +11,11 @@ tychobra_title <- function(title) {
   shiny::titlePanel(
     htmltools::HTML(
       paste0(
-        htmltools::tags$a(
-          href = 'https://cloud.tychobra.com/',
-          htmltools::tags$img(
-            src="https://res.cloudinary.com/dxqnb8xjb/image/upload/v1509563497/tychobra-logo-blue_dacbnz.svg",
-            height = '50px',
-            alt='Tychobra Logo',
-            style='margin-top: -20px; float: left;'
-          )
+        htmltools::tags$img(
+          src="https://res.cloudinary.com/dxqnb8xjb/image/upload/v1509563497/tychobra-logo-blue_dacbnz.svg",
+          height = '50px',
+          alt='Tychobra Logo',
+          style='margin-top: -20px; float: left;'
         ),
         htmltools::tags$span(title, style='float: left; font-size: 40px !important; margin-top: -18px !important; margin-left: 10px; padding-top: 0 !important;')
       )
