232319abe8356985e599ccae686c61ab4b295b9d
merlinoa <merlinoa88@gmail.com>
2018-07-19 17:25:14 -0400

    added argument to show or not show Tychobra logo in browser tab logo

--- R/use-tychobra-js.R
+++ R/use-tychobra-js.R
@@ -2,16 +2,19 @@
 #'
 #' function to load some custom js \code{shiny} and \code{shinydashboard} apps
 #'
+#' @param show_window_icon boolean that deafults to `TRUE`.  Determines whether or
+#' not to show Tychobra logo icon in browser tab
+#'
 #' @import shiny
 #'
 #' @export
 #'
-use_tychobra_js <- function() {
+use_tychobra_js <- function(show_window_icon = TRUE) {
 
   shiny::addResourcePath("tychobratools", system.file("srcjs", package = "tychobratools"))
 
-  return(
-    tags$div(
+  if (show_window_icon) {
+    out <- tags$div(
       shiny::singleton(
         shiny::tags$head(
           shiny::tags$link(rel="icon", href="https://res.cloudinary.com/dxqnb8xjb/image/upload/v1510505618/tychobra-logo-blue_d2k9vt.png"),
@@ -25,5 +28,21 @@ use_tychobra_js <- function() {
         )
       )
     )
-  )
+  } else {
+    out <- tags$div(
+      shiny::singleton(
+        shiny::tags$head(
+          shiny::tags$script(
+            src = file.path("tychobratools", "tychobra-shinydashboard.js")
+          ),
+          shiny::tags$link(
+            rel = "stylesheet",
+            href = file.path("tychobratools", "loading.css")
+          )
+        )
+      )
+    )
+  }
+
+  out
 }
