c81281f9a5418c824ca4b5ae2349209a288edf5a
RichardHHill <richard_hill@brown.edu>
2019-08-06 14:41:48 -0400

    Added icon color and formatted code

--- R/value_box_module.R
+++ R/value_box_module.R
@@ -5,13 +5,17 @@
 #' @param textColor A hex color code string
 #' @param width A number between 1 and 12
 #' @param href A url
+#' @param icon_color A valid color string
 #' 
 #' @import shiny
 #' @import shinydashboard
 #' 
 #' @export
 #'
-value_box_module_ui <- function(id, icon = NULL, backgroundColor = "#7cb5ec", textColor = "#FFF", width = 4, href = NULL) {
+value_box_module_ui <- function(
+  id, icon = NULL, backgroundColor = "#7cb5ec", textColor = "#FFF",
+  width = 4, href = NULL, icon_color = rgb(0, 0, 0, 0.15)
+) {
   ns <- NS(id)
 
   boxContent <- shiny::div(
@@ -28,13 +32,11 @@ value_box_module_ui <- function(id, icon = NULL, backgroundColor = "#7cb5ec", te
         style = "font-size: 15px; height: 20.91px; margin-bottom: 10px"
       )
     ),
-    if (!is.null(icon)) {
-      shiny::div(class = "icon-large", icon)
-    }
+    if (!is.null(icon)) shiny::div(class = "icon-large", style = paste0("color: ", icon_color, ";"), icon)
   )
-  if (!is.null(href)) {
-    boxContent <- shiny::a(href = href, boxContent)
-  }
+  
+  if (!is.null(href)) boxContent <- shiny::a(href = href, boxContent)
+  
   shiny::div(
     class = if (!is.null(width)) paste0("col-sm-", width),
     boxContent
@@ -50,21 +52,13 @@ value_box_module_ui <- function(id, icon = NULL, backgroundColor = "#7cb5ec", te
 #' 
 value_box_module <- function(input, output, server, value, subtitle) {
   value_prep <- reactive({
-    if (is.null(value())) {
-      ""
-    } else {
-      value()
-    }
+    if (is.null(value())) "" else value()
   })
   
   output$value <- shiny::renderText(value_prep())
   
   subtitle_prep <- reactive({
-    if (is.null(subtitle())) {
-      ""
-    } else {
-      subtitle()
-    }
+    if (is.null(subtitle())) "" else subtitle()
   })
   
   output$subtitle <- shiny::renderText(subtitle_prep())
