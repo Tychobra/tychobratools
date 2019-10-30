83f9b5e1bfa744f0d07c360bb112a9dd559584ab
merlinoa <andy.merlino@tychobra.com>
2019-10-30 00:32:56 -0400

    new `db_pool()` function

--- R/db_connect.R
+++ R/db_connect.R
@@ -15,25 +15,58 @@
 #'
 db_connect <- function(db_config, db_driver = RPostgres::Postgres()) {
 
-  if (is.null(db_config)) {
-    stop("Missing or invalid config file")
-  } else {
-    conn <- DBI::dbConnect(
-      db_driver,
-      dbname =  db_config$dbname,
-      user = db_config$user,
-      host = db_config$host,
-      password = db_config$password
-    )
 
-    # make sure we disconnect the database connection when the session ends
-    try({
-      shiny::onStop(function() {
-        DBI::dbDisconnect(conn)
-      })
+  conn <- DBI::dbConnect(
+    db_driver,
+    dbname =  db_config$dbname,
+    user = db_config$user,
+    host = db_config$host,
+    password = db_config$password
+  )
+
+  # make sure we disconnect the database connection when the session ends
+  try({
+    shiny::onStop(function() {
+      DBI::dbDisconnect(conn)
     })
+  })
 
-  }
 
   conn
+}
+
+
+#' Database connection pool module
+#'
+#' create a database connection pool
+#'
+#' @param db_config database configuration
+#' @param db_driver database driver
+#'
+#' @importFrom RPostgres Postgres
+#' @importFrom pool dbPool poolClose
+#' @importFrom shiny onStop
+#'
+#' @export
+#'
+#' @return a database pool
+#'
+db_pool <- function(db_config, db_driver = RPostgres::Postgres()) {
+
+  pool <- pool::dbPool(
+    db_driver,
+    dbname =  db_config$dbname,
+    user = db_config$user,
+    host = db_config$host,
+    password = db_config$password
+  )
+
+  # make sure we disconnect the database connection when the session ends
+  try({
+    shiny::onStop(function() {
+      pool::poolClose(conn)
+    })
+  })
+
+  pool
 }
