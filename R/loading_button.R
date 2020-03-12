5d370e0aa03af8de0d0f8205c4ad09a546654d88
phoward38 <patrick.howard@tychobra.com>
2020-03-12 19:33:09 -0400

    Initial commit for loading_button

---
+++ R/loading_button.R
@@ -0,0 +1,88 @@
+#' loading_button
+#'
+#' Button that becomes disabled until reset w/ `reset_loading_button`
+#'
+#' @param id the flipbox id
+#' @param label the button text (label)
+#'
+#' @export
+#'
+#' @importFrom htmltools tags
+#' @family flip_box functions
+loading_button <- function(id, 
+                           label,
+                           loading_text = "Loading...",
+                           loading_color = "grey"
+) {
+  
+  htmltools::tagList(
+    tags$button(
+      id = paste0("loading_button-", id),
+      class = "btn",
+      label
+    ),
+    tagList(
+      shiny::singleton(
+        tags$head(
+          tags$style(
+            paste0(
+              "
+              #loading_button-", id, " {
+                color: #fff;
+                background-color: #337ab7;
+                border-color: #2e6da4;
+              }
+              
+              .loading {
+                color: #fff;
+                background-color: ", loading_color, ";
+                border-color: ", loading_color, ";
+              }
+              "
+            )
+          ),
+          # Script for disabling button & changing text
+          tags$script(
+            paste0(
+              "$(function() {
+                
+                var counter = 0;
+              
+                $('#loading_button-", id, "').click(function() {
+                  Shiny.setInputValue('", id, "', true, { priority: 'event' });
+                  $(this).attr('disabled', true);
+                  $(this).html('", loading_text, "');
+                  $(this).css({'background-color': '", loading_color, "', 'border-color': '", loading_color, "'});
+                });
+              });"
+            )
+          ),
+          # Script for resetting button
+          tags$script(
+            paste0(
+              "Shiny.addCustomMessageHandler('reset_loading_button', function(message) {
+                $('#loading_button-' + message.id).attr('disabled', false);
+                $('#loading_button-' + message.id).html('", label, "');
+                $('#loading_button-' + message.id).css({'color': '#fff', 'background-color': '#337ab7', 'border-color': '#2e6da4'});
+                // Shiny.setInputValue(message.id, null);
+                // Shiny.setInputValue('#loading_button-' + message.id, 'TRUE');
+                // debugger;
+              });"
+            )
+          )
+        )
+      )
+    )
+  )
+}
+
+reset_loading_button <- function(id, session = shiny::getDefaultReactiveDomain()) {
+  
+  session$sendCustomMessage(
+    'reset_loading_button', 
+    message = list(
+      id = session$ns(id)
+    )
+  )
+}
+
