597af0ed5045b2cbdc68c4dc2df2413e155a04c7
merlinoa <andy.merlino@tychobra.com>
2020-07-01 14:32:52 -0400

    minor updates + docs for new `deploy_app()`

--- R/deploy_app.R
+++ R/deploy_app.R
@@ -1,52 +1,50 @@
-#'
-#'
 #' Deploy Shiny application
-#' 
+#'
 #' Deploy a Shiny application to a GCP VM instance (Must have gcloud on local machine).
-#' Currently, this function MUST be run with the application in a `shiny_app` directory, & 
+#' Currently, this function MUST be run with the application in a `shiny_app` directory, &
 #' that directory should be in the Current Working Directory.
-#' 
-#' @param gcloud Absolute path for gcloud CLI (Ex: `/usr/local/bin/google-cloud-sdk/bin`)
+#'
 #' @param deployed_dir_name Name of the directory in the VM instance for this app
 #' @param instance_name The name of the VM instance
 #' @param project_name The name of the GCP project for this VM instance
 #' @param project_zone The zone of the GCP project for this VM instance
-#' 
-#' @importFrom config get
-#' 
-#' @section Example:
-#' \preformatted{
+#' @param gcloud Absolute path for gcloud CLI (Ex: `/usr/local/bin/google-cloud-sdk/bin`)
+#'
+#'
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
 #'   deploy_app(
-#'     gcloud = '/usr/local/bin/google-cloud-sdk/bin',
 #'     deployed_dir_name = 'example_app',
 #'     instance_name = 'instance-1',
 #'     project_name = 'gcp-project',
-#'     project_zone = 'us-east1-d'
+#'     project_zone = 'us-east1-d',
+#'     gcloud = '/usr/local/bin/google-cloud-sdk/bin'
 #'   )
-#' 
 #' }
-#' 
-#' @export
-#' 
+#'
+#'
 deploy_app <- function(
-  gcloud = NULL,
-  deployed_dir_name = config::get(file = 'shiny_app/config.yaml')$app_name,
-  instance_name = NULL,
-  project_name = 'postgres-db-189513',
-  project_zone = 'us-east1-d'
+  deployed_dir_name,
+  instance_name,
+  project_name,
+  project_zone,
+  gcloud = '/usr/local/bin/google-cloud-sdk/bin'
 ) {
-  
+
   if (!is.null(gcloud)) {
     Sys.setenv(PATH = paste(Sys.getenv('PATH'), gcloud, sep = ":"))
   }
-  
+
   if (is.null(deployed_dir_name)) {
     stop("Argument `deployed_dir_name` is NULL")
   }
-  
+
   # create new "restart.txt" file so that the app automatically restarts once deployed
   write(NULL, file = "shiny_app/restart.txt")
-  
+
   # gcloud so set the project
   command_set_proj <- paste0("gcloud config set project ", project_name)
   system(command_set_proj)
